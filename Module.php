953518fef7bfb718a74401f977f6b081f2b32650
christophe robert <buse974@localhost.localdomain>
2018-09-01 12:09:27 +0200

    add getAutoloaderConfig

--- Module.php
+++ Module.php
@@ -11,6 +11,11 @@ use Dms\Service\DmsService;
 
 class Module implements ConfigProviderInterface
 {
+    public function getAutoloaderConfig()
+    {
+        return array('Zend\Loader\StandardAutoloader' => array('namespaces' => array(__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__)));
+    }
+    
     public function getConfig()
     {
         return include __DIR__.'/config/module.config.php';
